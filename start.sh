3b7c01d020eada67d59dcb6b2bc10ee71953d3c1
9018 <56663721+9018@users.noreply.github.com>
2022-04-27 21:45:07 +0800

    +elk
    +elk

--- start.sh
+++ start.sh
@@ -5,3 +5,7 @@ mkdir /home/data
 mkdir /home/data/grafana
 chown -R 472:472 /home/data/grafana/
 docker-compose up -d 
+cd /root
+git clone https://github.com/deviantony/docker-elk.git
+cd docker-elk
+docker-compose up -d

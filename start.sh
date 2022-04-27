71f0027a6cfba0d8751d5a12faf8dc457e665a01
9018 <56663721+9018@users.noreply.github.com>
2022-04-27 21:38:48 +0800

    1
    1

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 cd /root
 git clone https://github.com/9018/zabbixdocker.git
 cd zabbixdocker
-mkdir /home/data/grafana/
+mkdir /home/data
+mkdir /home/data/grafana
 chown -R 472:472 /home/data/grafana/
 docker-compose up -d 

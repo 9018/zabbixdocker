489b6a45e53402a9e0f5009d8e32f5c893a47097
9018 <56663721+9018@users.noreply.github.com>
2022-04-27 21:58:53 +0800

    1
    1

--- start.sh
+++ start.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+SHELL=/bin/sh
+export PATH
+os=$(egrep -i 'debian|ubuntu|cent' -o -- /etc/issue)
+os="${os,,}"
+if [ -z "$os" ] && type yum; then os='cent'; fi
+if [ "$os" = 'cent' ]; then
+  yum update
+  yum install -y epel-release
+  yum install -y wget git python-pip docker
+  systemctl start docker.service
+  systemctl enable docker.service
+  pip install --upgrade pip
+  pip install docker-compose --ignore-installed requests
+else
+  apt-get update
+  apt-get install -y docker
+fi
 cd /root
 git clone https://github.com/9018/zabbixdocker.git
 cd zabbixdocker

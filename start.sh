8581d8ce89d818172bbc2c3d671bb6d249047c86
9018 <56663721+9018@users.noreply.github.com>
2022-06-17 00:59:22 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,8 +11,8 @@ if [ "$os" = 'cent' ]; then
   yum install -y wget git python-pip docker
   systemctl start docker.service
   systemctl enable docker.service
-  pip install --upgrade pip
-  pip install docker-compose --ignore-installed requests
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
 else
   apt-get update
   apt-get install -y docker.io
@@ -27,10 +27,10 @@ cd zabbixdocker
 chown -R 472:472 grafana
 chmod -R 755 grafana/plugins/alexanderzobnin-zabbix-app/zabbix-plugin_linux_arm64
 chmod -R 755 grafana/plugins/alexanderzobnin-zabbix-app/zabbix-plugin_linux_amd64
-mv /etc/docker/daemon.json daemon.json.bak
-cp daemon.json /etc/docker/daemon.json
-systemctl daemon-reload
-systemctl restart docker
+#mv /etc/docker/daemon.json daemon.json.bak
+#cp daemon.json /etc/docker/daemon.json
+#systemctl daemon-reload
+#systemctl restart docker
 mkdir /home/data
 mkdir /home/data/grafana
 chown -R -v 472:472 /home/data/grafana/

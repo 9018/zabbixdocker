2a20ed7dc8ae20f95c8aafedc1ddf59abfc1f864
9018 <56663721+9018@users.noreply.github.com>
2022-06-24 14:03:06 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,7 +15,7 @@ if [ "$os" = 'cent' ]; then
   yum install -y wget git python-pip
   systemctl start docker.service
   systemctl enable docker.service
-  sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/2.6.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 else
   curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add –
